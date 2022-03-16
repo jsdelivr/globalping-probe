70c82ba938cab8b15fd63cf6a58f8f1137f3c4db
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-16 10:45:38 +0000

    Remove network-based test

--- test/unit/cmd/trace.test.ts
+++ test/unit/cmd/trace.test.ts
@@ -12,27 +12,6 @@ describe('trace command', () => {
 		sandbox.reset();
 	});
 
-	describe.skip('live', () => {
-		it('should run and parse trace - google.com', async () => {
-			const options = {
-				target: 'google.com',
-				port: 53,
-				protocol: 'UDP',
-			};
-
-			const cmd = traceCmd;
-
-			const trace = new TracerouteCommand(cmd);
-			await trace.run(mockSocket as any, 'measurement', 'test', options);
-
-			expect(mockSocket.emit.lastCall.args[0]).to.equal('probe:measurement:result');
-			expect(mockSocket.emit.lastCall.args[1]).to.have.property('result');
-			expect(mockSocket.emit.lastCall.args[1]).to.have.nested.property('result.destination');
-			expect(mockSocket.emit.lastCall.args[1]).to.have.nested.property('result.hops');
-			expect(mockSocket.emit.lastCall.args[1]).to.have.nested.property('result.hops[0].host');
-		}).timeout(5000);
-	});
-
 	describe('mock', () => {
 		const testCases = ['trace-success-linux'];
 
