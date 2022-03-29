81e249d67e0fb115cdde33f602b9c583f0858ce3
Artem Stoianov <zarianec91@gmail.com>
2022-03-29 11:05:34 +0200

    fix: add version check script

--- Dockerfile
+++ Dockerfile
@@ -18,13 +18,12 @@ RUN apt update && apt install -y iputils-ping traceroute dnsutils curl jq tini \
     && apt clean && apt autoremove -y \
     && rm -rf /var/lib/{apt,dpkg,cache,log}/
 
-WORKDIR /app
 COPY --from=builder /app/dist /app/dist
 COPY --from=builder /app/config /app/config
 COPY --from=builder /app/package.json /app/package-lock.json /app/
 COPY bin/entrypoint.sh /entrypoint.sh
 
-RUN npm install --production
+RUN cd /app && npm install --production
 
 ENTRYPOINT ["/usr/bin/tini", "--"]
 
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,14 +5,21 @@ function run_probe() {
   return
 }
 
+echo "Checking for the latest version"
+
 response=$(curl -XGET -s "https://api.github.com/repos/jsdelivr/globalping-probe/releases/latest")
 latestVersion=$(jq -r ".tag_name" <<<"${response}" | sed 's/v//')
 latestBundle=$(jq -r ".assets[] .browser_download_url" <<<"${response}")
 
 currentVersion=$(jq -r ".version" "/app/package.json")
 
+echo "Current version $currentVersion"
+echo "Latest version $latestVersion"
+
 if [ "$latestVersion" != "$currentVersion" ]; then
   loadedTarball="globalping-probe-${latestVersion}"
+
+  echo "Start self-update process"
 
   curl -Ls -XGET "${latestBundle}" -o "/tmp/${loadedTarball}.tar.gz"
   tar -xzf "/tmp/${loadedTarball}.tar.gz" --one-top-level="/tmp/${loadedTarball}"
@@ -21,6 +28,8 @@ if [ "$latestVersion" != "$currentVersion" ]; then
   mv "/tmp/${loadedTarball}" "/app"
 
   rm -rf "/tmp/${loadedTarball}.tar.gz"
+
+  echo "Self-update finished"
 fi
 
 run_probe
