0fbb774ac35e226c94ba660bca440e2f9403ff8c
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-04-04 13:11:22 +0000

    ref dns success test + add resolver error test

--- test/unit/cmd/dns.test.ts
+++ test/unit/cmd/dns.test.ts
@@ -4,7 +4,7 @@ import {Socket} from 'socket.io-client';
 import {getCmdMock, getCmdMockResult} from '../../utils.js';
 import {DnsCommand} from '../../../src/command/dns-command.js';
 
-describe.only('dns command', () => {
+describe('dns command', () => {
 	const sandbox = sinon.createSandbox();
 	const mockSocket = sandbox.createStubInstance(Socket);
 
@@ -12,30 +12,49 @@ describe.only('dns command', () => {
 		sandbox.reset();
 	});
 
-	describe('mock', () => {
-		const testCases = ['dns-success-linux'];
+	it('should parse trace - dns-success-linux', async () => {
+		const testCase = 'dns-success-linux';
+		const options = {
+			target: 'google.com',
+			query: {
+				type: 'TXT',
+			},
+		};
 
-		for (const testCase of testCases) {
-			it(`should parse trace - ${testCase}`, async () => {
-				const options = {
-					target: 'google.com',
-					query: {
-						type: 'TXT',
-					},
-				};
+		const rawOutput = getCmdMock(testCase);
+		const expectedResult = getCmdMockResult(testCase);
 
-				const rawOutput = getCmdMock(testCase);
-				const expectedResult = getCmdMockResult(testCase);
+		const mockCmd = Promise.resolve({stdout: rawOutput});
 
-				const mockCmd = Promise.resolve({stdout: rawOutput});
+		const dns = new DnsCommand((): any => mockCmd);
+		await dns.run(mockSocket as any, 'measurement', 'test', options);
 
-				const ping = new DnsCommand((): any => mockCmd);
-				await ping.run(mockSocket as any, 'measurement', 'test', options);
+		expect(mockSocket.emit.calledOnce).to.be.true;
+		expect(mockSocket.emit.firstCall.args[0]).to.equal('probe:measurement:result');
+		expect(mockSocket.emit.firstCall.args[1]).to.deep.equal(expectedResult);
+	});
 
-				expect(mockSocket.emit.calledOnce).to.be.true;
-				expect(mockSocket.emit.firstCall.args[0]).to.equal('probe:measurement:result');
-				expect(mockSocket.emit.firstCall.args[1]).to.deep.equal(expectedResult);
-			});
-		}
+	it('should return ExecaError - dns-success-linux', async () => {
+		const testCase = 'dns-resolver-error-linux';
+		const options = {
+			target: 'google.com',
+			query: {
+				type: 'TXT',
+				resolver: 'sdsa',
+			},
+		};
+
+		const rawOutput = getCmdMock(testCase);
+		const expectedResult = getCmdMockResult(testCase);
+
+		// eslint-disable-next-line prefer-promise-reject-errors
+		const mockCmd = Promise.reject({stderr: rawOutput});
+
+		const dns = new DnsCommand((): any => mockCmd);
+		await dns.run(mockSocket as any, 'measurement', 'test', options);
+
+		expect(mockSocket.emit.calledOnce).to.be.true;
+		expect(mockSocket.emit.firstCall.args[0]).to.equal('probe:measurement:result');
+		expect(mockSocket.emit.firstCall.args[1]).to.deep.equal(expectedResult);
 	});
 });
