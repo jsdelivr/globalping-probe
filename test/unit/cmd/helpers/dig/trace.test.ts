b26a4e7af4e924b61b59f55d15aa63e0f0adcaed
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-04-27 06:06:15 +0000

    add: trace dig parser

--- test/unit/cmd/helpers/dig/trace.test.ts
+++ test/unit/cmd/helpers/dig/trace.test.ts
@@ -1,18 +1,18 @@
 import {expect} from 'chai';
 import {
-  TraceDigParser,
-  DnsParseResponse
+	TraceDigParser,
+	DnsParseResponse,
 } from '../../../../../src/command/handlers/dig/trace.js';
 import {getCmdMock} from '../../../../utils.js';
 
-describe.only('dig trace helper', () => {
+describe('dig trace helper', () => {
 	describe('parse', () => {
 		it('should succeed', () => {
 			const rawOutput = getCmdMock('dig/trace.success');
-      const parsedOutput = TraceDigParser.parse(rawOutput);
+			const parsedOutput = TraceDigParser.parse(rawOutput);
 
-      expect(parsedOutput).to.not.be.instanceof(Error);
-      expect((parsedOutput as DnsParseResponse).result.length).to.equal(3);
+			expect(parsedOutput).to.not.be.instanceof(Error);
+			expect((parsedOutput as DnsParseResponse).result.length).to.equal(4);
 		});
 	});
 });
