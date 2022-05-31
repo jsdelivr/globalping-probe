fd813530c9a2b0d6c921354b8f1bfa5d95671bfa
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-05-31 17:54:02 +0000

    ref: http tests

--- test/unit/cmd/http.test.ts
+++ test/unit/cmd/http.test.ts
@@ -5,6 +5,53 @@ import {expect} from 'chai';
 import {Socket} from 'socket.io-client';
 import {HttpCommand} from '../../../src/command/http-command.js';
 
+type StreamCert = {
+	valid_from: number;
+	valid_to: number;
+	issuer: {
+		[k: string]: string;
+		CN: string;
+	};
+	subject: {
+		[k: string]: string;
+		CN: string;
+	};
+	subjectaltname: string; // 'DNS:*.google.com, DNS:google.com'
+};
+
+type StreamResponse = {
+	timings: {
+		[k: string]: number | Record<string, number>;
+		phases?: Record<string, number>;
+	};
+	socket: {
+		authorized?: boolean;
+		authorizationError?: string;
+		cert?: StreamCert;
+		getPeerCertificate?: () => StreamCert;
+	};
+};
+
+class Stream {
+	response: StreamResponse | undefined;
+	stream: PassThrough;
+
+	constructor(
+		response: StreamResponse,
+	) {
+		this.stream = new PassThrough();
+		this.response = response;
+	}
+
+	on(key: string, fn: (..._args: any[]) => void) {
+		this.stream.on(key, fn);
+	}
+
+	emit(key: string, data?: any) {
+		this.stream.emit(key, data);
+	}
+}
+
 describe('http command', () => {
 	const sandbox = sinon.createSandbox();
 	const mockedSocket = sandbox.createStubInstance(Socket);
@@ -26,16 +73,28 @@ describe('http command', () => {
 
 		const events = {
 			response: {
+				socket: {},
 				statusCode: 200,
 				httpVersion: '1.1',
 				timings: {
 					start: 0,
-					response: 10,
+					connect: 1,
+					response: 11,
+					phases: {
+						dns: 5,
+						download: 10,
+						total: 11,
+						firstByte: 1,
+					},
 				},
 				headers: {test: 'abc'},
 				rawHeaders: ['test', 'abc'],
 			},
 			data: ['abc', 'def', 'ghi'],
+		};
+
+		const response = {
+			...events.response,
 		};
 
 		const expectedResult = {
@@ -44,16 +103,24 @@ describe('http command', () => {
 				headers: {
 					test: 'abc',
 				},
-				responseTime: 10,
+				timings: {
+					dns: 5,
+					connect: 1,
+					response: 11,
+					download: 10,
+					firstByte: 1,
+					total: 11,
+				},
 				rawHeaders: 'test: abc',
 				rawBody: 'abcdefghi',
 				rawOutput: 'abcdefghi',
 				statusCode: 200,
+				tls: {},
 			},
 			testId: 'test',
 		};
 
-		const stream = new PassThrough();
+		const stream = new Stream(response);
 		const httpCmd = (): Request => stream as never;
 
 		const http = new HttpCommand(httpCmd);
@@ -86,15 +153,27 @@ describe('http command', () => {
 
 		const events = {
 			response: {
+				socket: {},
 				statusCode: 200,
 				timings: {
 					start: 0,
-					response: 10,
+					response: 200,
+					connect: 100,
+					phases: {
+						download: 10,
+						total: 11,
+						dns: 5,
+						firstByte: 1,
+					},
 				},
 				httpVersion: '1.1',
 				headers: {test: 'abc'},
 				rawHeaders: ['test', 'abc'],
 			},
+		};
+
+		const response = {
+			...events.response,
 		};
 
 		const expectedResult = {
@@ -103,16 +182,24 @@ describe('http command', () => {
 				headers: {
 					test: 'abc',
 				},
-				responseTime: 10,
+				timings: {
+					connect: 100,
+					response: 200,
+					dns: 5,
+					total: 11,
+					firstByte: 1,
+					download: 10,
+				},
 				rawHeaders: 'test: abc',
 				rawBody: '',
 				rawOutput: 'HTTP/1.1 200\ntest: abc',
 				statusCode: 200,
+				tls: {},
 			},
 			testId: 'test',
 		};
 
-		const stream = new PassThrough();
+		const stream = new Stream(response);
 		const httpCmd = (): Request => stream as never;
 
 		const http = new HttpCommand(httpCmd);
@@ -136,17 +223,46 @@ describe('http command', () => {
 			},
 		};
 
+		/* eslint-disable @typescript-eslint/naming-convention */
+		const cert = {
+			valid_from: 100,
+			valid_to: 200,
+			issuer: {
+				CN: 'abc ltd',
+			},
+			subject: {
+				CN: 'defllc.dom',
+			},
+			subjectaltname: 'DNS:defllc.com, DNS:*.defllc.com',
+		};
+		/* eslint-enable @typescript-eslint/naming-convention */
+
 		const events = {
 			response: {
+				socket: {
+					authorized: true,
+					getPeerCertificate: () => cert,
+				},
 				statusCode: 200,
 				timings: {
 					start: 0,
-					response: 10,
+					response: 200,
+					connect: 100,
+					phases: {
+						download: 10,
+						total: 11,
+						dns: 5,
+						firstByte: 1,
+					},
 				},
 				httpVersion: '2',
 				headers: {test: 'abc'},
 				rawHeaders: [':status', 200, 'test', 'abc'],
 			},
+		};
+
+		const response = {
+			...events.response,
 		};
 
 		const expectedResult = {
@@ -155,7 +271,26 @@ describe('http command', () => {
 				headers: {
 					test: 'abc',
 				},
-				responseTime: 10,
+				timings: {
+					connect: 100,
+					response: 200,
+					total: 11,
+					dns: 5,
+					firstByte: 1,
+					download: 10,
+				},
+				tls: {
+					authorized: true,
+					createdAt: 100,
+					expireAt: 200,
+					issuer: {
+						...cert.issuer,
+					},
+					subject: {
+						...cert.subject,
+						alt: cert.subjectaltname,
+					},
+				},
 				rawHeaders: ':status: 200\ntest: abc',
 				rawBody: '',
 				rawOutput: 'HTTP/2 200\ntest: abc',
@@ -164,7 +299,7 @@ describe('http command', () => {
 			testId: 'test',
 		};
 
-		const stream = new PassThrough();
+		const stream = new Stream(response);
 		const httpCmd = (): Request => stream as never;
 
 		const http = new HttpCommand(httpCmd);
@@ -189,7 +324,19 @@ describe('http command', () => {
 		};
 
 		const events = {
+			response: {
+				timings: {
+					phases: {
+						download: 10,
+						total: 11,
+					},
+				},
+			},
 			error: new Error('ENODATA google.com'),
+		};
+
+		const response = {
+			...events.response,
 		};
 
 		const expectedResult = {
@@ -198,14 +345,19 @@ describe('http command', () => {
 				headers: {},
 				rawHeaders: '',
 				rawBody: '',
-				responseTime: 0,
+				timings: {
+					download: 10,
+					total: 11,
+				},
+				tls: {},
 				rawOutput: 'ENODATA google.com',
 				statusCode: 0,
 			},
 			testId: 'test',
 		};
 
-		const stream = new PassThrough();
+		const stream = new Stream(response);
+
 		const httpCmd = (): Request => stream as never;
 
 		const http = new HttpCommand(httpCmd);
