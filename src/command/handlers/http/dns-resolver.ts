4b833466628b5263f4ff13725b319a59187926d3
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-06-07 15:14:44 +0100

    feat: http command (#47)
    * add: http command
    
    * ref: unecessary method remap
    
    * add: dns lookup method + private IP check
    
    * ref: rename result publishing method name
    
    * add: dnslookup: support dnsServer override
    
    * ref: cleanup
    
    * ref: making deepsource happy
    
    * fix: dnslookup: support optional ipv6 (default ipv4)
    
    * ref: prepare dnsLookup for unit tests
    
    * ref: making eslint + deepsource happy
    
    * ref: making eslint + deepsource happy
    
    * fix: hard-coded limit
    
    * add: unit test dns lookup (filter private ip)
    
    * Add: http unit tests
    
    * add: httpCmd: support optional resolverFn argument
    
    * fix: making deepsource happy
    
    * ref: test: rename http dns helper filename
    
    * add: http version +_response time data
    
    * add: hide :status header
    
    * ref: dns: use promises + add callbackify method
    
    * ref: move callbackify to lib/util.ts file
    
    * add: tls data to the response
    
    * add: expand timings object
    
    * ref: http tests
    
    * making linters happy
    
    * making linters happy
    
    * making linters happy
    
    * fix: schema: optional query obj

---
+++ src/command/handlers/http/dns-resolver.ts
@@ -0,0 +1,45 @@
+import dns, {RecordWithTtl} from 'node:dns';
+import isIpPrivate from 'private-ip';
+
+type IpFamily = 4 | 6;
+type Options = {
+	family: IpFamily;
+};
+type ErrnoException = NodeJS.ErrnoException;
+export type ResolverOptionsType = {ttl: boolean};
+export type ResolverType = (hostname: string, options: ResolverOptionsType) => Promise<string[]>;
+
+const isRecordWithTtl = (record: unknown): record is RecordWithTtl => Boolean((record as RecordWithTtl).ttl);
+
+export const buildResolver = (resolverAddr: string | undefined, family: IpFamily): ResolverType => {
+	const resolver = new dns.promises.Resolver();
+
+	if (resolverAddr) {
+		resolver.setServers([resolverAddr]);
+	}
+
+	const resolve = family === 6 ? resolver.resolve6.bind(resolver) : resolver.resolve4.bind(resolver);
+
+	return resolve;
+};
+
+export const dnsLookup = (resolverAddr: string | undefined, resolverFn?: ResolverType) => async (
+	hostname: string,
+	options: Options,
+): Promise<Error | ErrnoException | [string, number]> => {
+	const resolver = resolverFn ?? buildResolver(resolverAddr, options.family);
+
+	try {
+		const result = await resolver(hostname, {ttl: false});
+
+		const validIps = result.map(r => isRecordWithTtl(r) ? r.address : r).filter(r => !isIpPrivate(r));
+
+		if (validIps.length === 0) {
+			throw new Error(`ENODATA ${hostname}`);
+		}
+
+		return [validIps[0]!, options.family];
+	} catch (error: unknown) {
+		throw error as ErrnoException;
+	}
+};
---
+++ test/unit/cmd/helpers/http/dns.test.ts
@@ -0,0 +1,87 @@
+import {expect} from 'chai';
+import {callbackify} from '../../../../../src/lib/util.js';
+import {dnsLookup} from '../../../../../src/command/handlers/http/dns-resolver.js';
+import type {ResolverType, ResolverOptionsType} from '../../../../../src/command/handlers/http/dns-resolver.js';
+
+export const buildResolver = (ipList: string[]): ResolverType => async (_hostname: string, _options: ResolverOptionsType): Promise<string[]> => ipList;
+
+describe('http helper', () => {
+	describe('dns', () => {
+		it('should return an error (private ip)', async () => {
+			const data = {
+				ipList: ['192.168.0.1'],
+				hostname: 'abc.com',
+				options: {
+					family: 4,
+				},
+			};
+
+			const resolver = buildResolver(data.ipList);
+			const lookup = dnsLookup(undefined, resolver);
+
+			let response: unknown;
+
+			try {
+				response = await lookup(
+					data.hostname,
+					// @ts-expect-error family type error
+					data.options,
+				) as [string, number];
+			} catch (error: unknown) {
+				response = error;
+			}
+
+			expect(response).to.be.instanceof(Error);
+		});
+
+		it('should filter out private ip, return public ip', async () => {
+			const data = {
+				ipList: ['192.168.0.1', '1.1.1.1'],
+				hostname: 'abc.com',
+				options: {
+					family: 4,
+				},
+			};
+
+			const resolver = buildResolver(data.ipList);
+			const lookup = dnsLookup(undefined, resolver);
+
+			let response: unknown;
+
+			try {
+				response = await lookup(
+					data.hostname,
+					// @ts-expect-error family type error
+					data.options,
+				) as [string, number];
+			} catch (error: unknown) {
+				response = error;
+			}
+
+			expect(response).to.deep.equal(['1.1.1.1', 4]);
+		});
+
+		it('should pass - callbackify', done => {
+			const data = {
+				ipList: ['192.168.0.1', '1.1.1.1'],
+				hostname: 'abc.com',
+				options: {
+					family: 4,
+				},
+			};
+
+			const resolver = buildResolver(data.ipList);
+			const lookup = callbackify(dnsLookup(undefined, resolver), true);
+
+			lookup(
+				data.hostname,
+				data.options,
+				(_error: undefined, result: string, family: number) => {
+					expect(result).to.deep.equal('1.1.1.1');
+					expect(family).to.equal(4);
+					done();
+				},
+			);
+		});
+	});
+});
