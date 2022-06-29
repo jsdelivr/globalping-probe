f0365a53a81fc6fc4669f6816ac1c0a35c73528f
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-06-29 23:56:42 +0000

    fix: dev-mode: bypass the root requirement

--- src/command/mtr-command.ts
+++ src/command/mtr-command.ts
@@ -1,3 +1,4 @@
+import process from 'node:process';
 import dns from 'node:dns';
 import {isIP} from 'is-ip';
 import isIpPrivate from 'private-ip';
@@ -38,7 +39,7 @@ export const mtrCmd = (options: MtrOptions): ExecaChildProcess => {
 	const args = [
 		// Ipv4
 		'-4',
-		['--interval', '0.5'],
+		['--interval', process.env['NODE_ENV'] === 'development' ? '1' : '0.5'],
 		['--gracetime', '3'],
 		['--max-ttl', '30'],
 		['--timeout', '15'],
