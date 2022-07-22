5ed80644dcd81b6951e0035997bfd6ba0b4d46b2
DecliningLotus <declininglotus@gmail.com>
2022-07-22 17:39:16 +0300

    fix: enforce maxsockets infinity according to spec

--- src/command/http-command.ts
+++ src/command/http-command.ts
@@ -106,8 +106,11 @@ export const httpCmd = (options: HttpOptions, resolverFn?: ResolverType): Reques
 			downloadLimit: 10_000,
 		},
 		agent: {
-			http: new http.Agent({keepAlive: false}),
-			https: new https.Agent({maxCachedSessions: 0, keepAlive: false}),
+			// Ensure Connection: closed header is used - https://nodejs.org/api/http.html#new-agentoptions
+			/* eslint-disable unicorn/prefer-number-properties */
+			http: new http.Agent({keepAlive: false, maxSockets: Infinity}),
+			https: new https.Agent({maxCachedSessions: 0, keepAlive: false, maxSockets: Infinity}),
+			/* eslint-enable unicorn/prefer-number-properties */
 			http2: new http2.Agent({maxCachedTlsSessions: 1}),
 		},
 	};
