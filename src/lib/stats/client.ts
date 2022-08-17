8506ec96e78ee8d1a5e01f438415ba9ee6976176
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-08-17 10:51:57 +0000

    ref: interval method

--- src/lib/stats/client.ts
+++ src/lib/stats/client.ts
@@ -1,6 +1,9 @@
 import type {Socket} from 'socket.io-client';
 import {getConfValue} from '../config.js';
+import {scopedLogger} from '../logger.js';
 import {getCpuUsage} from './cpu.js';
+
+const logger = scopedLogger('probe:stats:report');
 
 type Worker = {
 	jobs: Map<string, number>;
@@ -20,7 +23,10 @@ export const report = async (socket: Socket, jobCount: number) => {
 };
 
 export const run = (socket: Socket, worker: Worker) => {
-	setInterval(async () => {
-		await report(socket, worker.jobs.size);
+	setInterval(() => {
+		report(socket, worker.jobs.size)
+			.catch((error: unknown) => {
+				logger.error('Unknown error', error);
+			});
 	}, statsConfig.interval * 1000);
 };
