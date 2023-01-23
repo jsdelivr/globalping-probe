132e934df2f8e22d8e4b194fa98bbb4140d2554d
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-01-23 14:17:25 +0300

    feat: add wallaby and move to ts-node + testdouble

---
+++ wallaby.js
@@ -0,0 +1,32 @@
+import * as path from 'node:path';
+import * as url from 'node:url';
+
+export default function wallaby() {
+	const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+	return {
+		testFramework: 'mocha',
+		files: [
+			'src/**/*.ts',
+			'config/*',
+			'test/mocks/**/*',
+			'test/utils.ts',
+			'package.json',
+		],
+		tests: [
+			'test/unit/**/*.test.ts',
+		],
+		env: {
+			type: 'node',
+			params: {
+				runner: '--experimental-specifier-resolution=node --loader '
+          + path.join(__dirname, 'node_modules/testdouble/lib/index.mjs'),
+				env: 'NODE_ENV=test;NEW_RELIC_ENABLED=false;NEW_RELIC_LOG_ENABLED=false',
+			},
+		},
+		preprocessors: {
+			'**/*.ts': file => file.content.replace(/\.ts/g, '.js'),
+		},
+		workers: {restart: true, initial: 1, regular: 1},
+		runMode: 'onsave',
+	};
+}
