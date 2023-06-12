f68d1b241b2780ff5053c32b7164a1a5eae2bb5a
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-06-12 19:16:30 +0300

    feat: change root server

--- test/unit/lib/status-manager.test.ts
+++ test/unit/lib/status-manager.test.ts
@@ -56,7 +56,7 @@ describe('StatusManager', () => {
 		pingCmd.rejects({ stdout: 'host not found' });
 		await statusManager.start();
 		expect(pingCmd.callCount).to.equal(3);
-		expect(pingCmd.args[0]).to.deep.equal([{ type: 'ping', target: 'l.root-servers.net', packets: 6, inProgressUpdates: false }]);
+		expect(pingCmd.args[0]).to.deep.equal([{ type: 'ping', target: 'a.root-servers.net', packets: 6, inProgressUpdates: false }]);
 		expect(pingCmd.args[1]).to.deep.equal([{ type: 'ping', target: 'k.root-servers.net', packets: 6, inProgressUpdates: false }]);
 		expect(pingCmd.args[2]).to.deep.equal([{ type: 'ping', target: 'j.root-servers.net', packets: 6, inProgressUpdates: false }]);
 		expect(statusManager.getStatus()).to.equal('ping-test-failed');
@@ -110,7 +110,7 @@ describe('StatusManager', () => {
 		const statusManager = initStatusManager(socket, pingCmd);
 		await statusManager.start();
 		expect(pingCmd.callCount).to.equal(3);
-		expect(pingCmd.args[0]).to.deep.equal([{ type: 'ping', target: 'l.root-servers.net', packets: 6, inProgressUpdates: false }]);
+		expect(pingCmd.args[0]).to.deep.equal([{ type: 'ping', target: 'a.root-servers.net', packets: 6, inProgressUpdates: false }]);
 		expect(pingCmd.args[1]).to.deep.equal([{ type: 'ping', target: 'k.root-servers.net', packets: 6, inProgressUpdates: false }]);
 		expect(pingCmd.args[2]).to.deep.equal([{ type: 'ping', target: 'j.root-servers.net', packets: 6, inProgressUpdates: false }]);
 		expect(statusManager.getStatus()).to.equal('ready');
