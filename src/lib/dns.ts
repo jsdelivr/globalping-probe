757c4cbb478125acb6173b98e1da90d48d511eaf
Sukka <isukkaw@gmail.com>
2023-07-30 19:30:23 +0800

    refactor: remove more dependencies (#173)
    Co-authored-by: Martin Kolárik <martin@kolarik.sk>

--- src/lib/dns.ts
+++ src/lib/dns.ts
@@ -1,17 +1,17 @@
 import dns from 'node:dns';
-import validator from 'validator';
+import { isIPv6 } from 'node:net';
 import { isIpPrivate } from './private-ip';
 
 export const getDnsServers = (getServers: () => string[] = dns.getServers): string[] => {
 	const servers = getServers();
 
 	return servers
-	// Filter out ipv6
+		// Filter out ipv6
 		.filter((addr: string) => {
 			const ipv6Match = /^\[(.*)]/g.exec(addr); // Nested with port
-			return !(validator.isIP(addr, 6) || validator.isIP(ipv6Match?.[1] ?? ''));
+			return !isIPv6(addr) && !isIPv6(ipv6Match?.[1] ?? '');
 		})
-	// Hide private ips
+		// Hide private ips
 		.map((addr: string) => {
 			const ip = addr.replace(/:\d{1,5}$/, '');
 			return isIpPrivate(ip) ? 'private' : addr;
