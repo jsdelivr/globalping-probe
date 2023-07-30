4f52d1b59c8909bb0362cf961c0cc809d3b8a469
Martin Kolárik <martin@kolarik.sk>
2023-07-30 13:27:58 +0200

    fix: filtering IPv6

--- src/lib/dns.ts
+++ src/lib/dns.ts
@@ -1,26 +1,17 @@
 import dns from 'node:dns';
-import { isIP, isIPv6 } from 'node:net';
+import { isIPv6 } from 'node:net';
 import { isIpPrivate } from './private-ip';
 
 export const getDnsServers = (getServers: () => string[] = dns.getServers): string[] => {
 	const servers = getServers();
 
 	return servers
-	// Filter out ipv6
+		// Filter out ipv6
 		.filter((addr: string) => {
-			if (isIPv6(addr)) {
-				return false;
-			}
-
 			const ipv6Match = /^\[(.*)]/g.exec(addr); // Nested with port
-
-			if (ipv6Match && ipv6Match[1]) {
-				return !isIP(ipv6Match[1]);
-			}
-
-			return true;
+			return !isIPv6(addr) && !isIPv6(ipv6Match?.[1] ?? '');
 		})
-	// Hide private ips
+		// Hide private ips
 		.map((addr: string) => {
 			const ip = addr.replace(/:\d{1,5}$/, '');
 			return isIpPrivate(ip) ? 'private' : addr;
