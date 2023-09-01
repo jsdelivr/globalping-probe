b5b43ad5a21d540f907fc2a8018bbe53f6a3b475
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-09-01 17:54:57 +0200

    feat: update fake ip generation 3

--- src/lib/fake-ip.ts
+++ src/lib/fake-ip.ts
@@ -1,21 +1,14 @@
-// import { expand } from 'cidr-tools';
-
-// let fakeIps: string[] = [];
-
-// const initFakeIps = () => {
-// 	const secondOctet = parseInt(process.env['FAKE_PROBE_IP']!, 10);
-// 	fakeIps = expand(`100.${secondOctet}.0.0/20`);
-// };
-
 export const getFakeIp = () => {
-	const octet1 = parseInt(process.env['FAKE_PROBE_IP']!, 10);
+	const octet1String = process.env['FAKE_PROBE_IP'];
+
+	if (!octet1String) {
+		throw new Error('FAKE_PROBE_IP is not specified');
+	}
+
+	const octet1 = parseInt(octet1String, 10);
 	const octet2 = Math.floor(process.pid / 65536) % 256; // Divide by 2^16
 	const octet3 = Math.floor(process.pid / 256) % 256; // Divide by 2^8
 	const octet4 = process.pid % 256;
 
 	return `${octet1}.${octet2}.${octet3}.${octet4}`;
 };
-
-// if (process.env['FAKE_PROBE_IP']) {
-// 	initFakeIps();
-// }
