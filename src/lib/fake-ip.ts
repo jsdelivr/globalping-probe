0c2ceee783a7ea5a2ae88b4b2fd9e5009faeb424
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-09-01 17:41:11 +0200

    feat: update fake ip generation 2

--- src/lib/fake-ip.ts
+++ src/lib/fake-ip.ts
@@ -2,24 +2,20 @@
 
 // let fakeIps: string[] = [];
 
-const initFakeIps = () => {
-	// const secondOctet = parseInt(process.env['FAKE_PROBE_IP']!, 10);
-	// fakeIps = expand(`100.${secondOctet}.0.0/20`);
-};
+// const initFakeIps = () => {
+// 	const secondOctet = parseInt(process.env['FAKE_PROBE_IP']!, 10);
+// 	fakeIps = expand(`100.${secondOctet}.0.0/20`);
+// };
 
 export const getFakeIp = () => {
-	const secondOctet = parseInt(process.env['FAKE_PROBE_IP']!, 10);
-	const thirdOctet = process.pid / 256 % 256;
-	const forthOctet = process.pid % 256;
+	const octet1 = parseInt(process.env['FAKE_PROBE_IP']!, 10);
+	const octet2 = Math.floor(process.pid / 65536) % 256; // Divide by 2^16
+	const octet3 = Math.floor(process.pid / 256) % 256; // Divide by 2^8
+	const octet4 = process.pid % 256;
 
-	return `100.${secondOctet}.${thirdOctet}.${forthOctet}`;
-	// if (fakeIps.length === 0) {
-	// 	throw new Error('Fake ips are not initialized.');
-	// }
-
-	// return fakeIps[process.pid % fakeIps.length];
+	return `${octet1}.${octet2}.${octet3}.${octet4}`;
 };
 
-if (process.env['FAKE_PROBE_IP']) {
-	initFakeIps();
-}
+// if (process.env['FAKE_PROBE_IP']) {
+// 	initFakeIps();
+// }
