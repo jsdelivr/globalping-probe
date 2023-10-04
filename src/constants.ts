d6f66875fabf83485c62522eca2b402ac6013e3c
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-10-04 13:43:46 +0200

    feat: rename id to uuid

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,6 @@ export const VERSION = pkg.version;
 
 export const NODE_VERSION = process.version;
 
-export const ID = randomUUID();
+export const UUID = randomUUID();
 
 export const PROGRESS_INTERVAL_TIME = 500;
