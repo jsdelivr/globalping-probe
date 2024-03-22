3d910874144fbb7d2dee34c69a961827820480b7
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-03-22 18:49:25 +0100

    feat: download updated e2e tests

--- config/test.cjs
+++ config/test.cjs
@@ -1,1 +1,19 @@
-module.exports = {};
+module.exports = {
+	redis: {
+		url: 'redis://localhost:6379',
+		socket: {
+			tls: false,
+		},
+	},
+	db: {
+		type: 'mysql',
+		connection: {
+			host: 'localhost',
+			user: 'directus',
+			password: 'password',
+			database: 'directus-test',
+			port: 3306,
+			multipleStatements: true,
+		},
+	},
+};
