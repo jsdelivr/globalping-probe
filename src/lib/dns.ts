42a3d86217b1c98b195aac432cc903ad940f0355
Martin Kolárik <martin@kolarik.sk>
2024-05-28 17:35:15 +0200

    Update src/lib/dns.ts
    Co-authored-by: Alex Yarmosh <yarmoshalexey@gmail.com>

--- src/lib/dns.ts
+++ src/lib/dns.ts
@@ -8,8 +8,8 @@ export const getDnsServers = (getServers: () => string[] = dns.getServers): stri
 	return servers
 		// Hide private ips
 		.map((addr: string) => {
-			const ip_ = addr.replace('[', '').replace(/]:\d{1,5}$/, ''); // removes port number if it is ipv6
-			const ip = isIPv6(ip_) ? ip_ : ip_.replace(/:\d{1,5}$/, ''); // removes port number if it is not ipv6
+			let ip = addr.replace('[', '').replace(/]:\d{1,5}$/, ''); // removes port number if it is ipv6
+			ip = isIPv6(ip) ? ip : ip.replace(/:\d{1,5}$/, ''); // removes port number if it is not ipv6
 			return isIpPrivate(ip) ? 'private' : addr;
 		});
 };
