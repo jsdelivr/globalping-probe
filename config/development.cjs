4dc411339d03959138e1e8c3a7f2c807a8610873
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-07-16 11:40:49 +0200

    feat: send http request

--- config/development.cjs
+++ config/development.cjs
@@ -1,6 +1,7 @@
 module.exports = {
 	api: {
 		host: 'ws://localhost:3000',
+		httpHost: 'https://localhost:3000/v1',
 	},
 	update: {
 		interval: 10,
--- src/helper/alt-ips-handler.ts
+++ src/helper/alt-ips-handler.ts
@@ -1,10 +1,12 @@
 import os from 'node:os';
+import config from 'config';
 import _ from 'lodash';
 import { scopedLogger } from '../lib/logger.js';
+import got from 'got';
 
 const logger = scopedLogger('api:connect:alt-ips-token');
 
-export const apiConnectAltIpsHandler = async ({ token }: { token: string }): Promise<void> => {
+export const apiConnectAltIpsHandler = async ({ token, socketId }: { token: string, socketId: string }): Promise<void> => {
 	const uniqIps = _(os.networkInterfaces())
 		.values()
 		.filter((int): int is os.NetworkInterfaceInfo[] => !!int)
@@ -14,6 +16,15 @@ export const apiConnectAltIpsHandler = async ({ token }: { token: string }): Pro
 		.map('address')
 		.value();
 
-	console.log('token', token);
-	console.log('uniqIps', uniqIps);
+	await Promise.all(uniqIps.map(ip => sendToken(ip, token, socketId)));
+};
+
+const sendToken = async (ip: string, token: string, socketId: string) => {
+	const data = await got.post(`${config.get<string>('api.httpHost')}/alternative-ip`, {
+		localAddress: ip,
+		json: {
+			token,
+			socketId,
+		},
+	});
 };
