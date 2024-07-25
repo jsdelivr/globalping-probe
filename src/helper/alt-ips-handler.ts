a4f78795dd5d66e3428fff315d5aad343769ad01
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-07-25 14:53:54 +0200

    feat: log probe ips

--- src/helper/alt-ips-handler.ts
+++ src/helper/alt-ips-handler.ts
@@ -2,46 +2,57 @@ import os from 'node:os';
 import config from 'config';
 import _ from 'lodash';
 import { scopedLogger } from '../lib/logger.js';
-import got, { type RequestError } from 'got';
+import got, { RequestError } from 'got';
 
 const logger = scopedLogger('api:connect:alt-ips-token');
 
-export const apiConnectAltIpsHandler = async ({ token, socketId }: { token: string, socketId: string }): Promise<void> => {
-	const uniqIps = _(os.networkInterfaces())
+export const apiConnectAltIpsHandler = async ({ token, socketId, ip }: { token: string, socketId: string, ip: string }): Promise<void> => {
+	const allIps = [ ip ];
+	const addresses = _(os.networkInterfaces())
 		.values()
 		.filter((int): int is os.NetworkInterfaceInfo[] => !!int)
 		.flatten()
 		.uniqBy('address')
 		.filter(address => !address.internal)
-		// .map('address')
+		.filter(address => !address.address.startsWith('fe80:'))
+		.filter(address => !address.address.startsWith('169.254.'))
 		.value();
 
-	console.log('uniqIps', uniqIps);
+	const results = await Promise.allSettled(addresses.map(({ address, family }) => sendToken(address, family === 'IPv6' ? 6 : 4, token, socketId)));
 
-	await Promise.all(uniqIps.map(({ address, family }) => sendToken(
-		address,
-		family === 'IPv6' ? 6 : 4,
-		token,
-		socketId,
-	)));
+	results.forEach((result) => {
+		if (result.status === 'rejected') {
+			logger.error(result.reason instanceof RequestError ? result.reason.message : result);
+		} else {
+			allIps.push(result.value);
+		}
+	});
+
+	const uniqIps = _(allIps).uniq().value();
+
+	if (uniqIps.length === 1) {
+		logger.info(`IP address of the probe: ${uniqIps[0]}`);
+	} else {
+		logger.info(`IP addresses of the probe: ${uniqIps.join(', ')}`);
+	}
 };
 
 const sendToken = async (ip: string, dnsLookupIpVersion: 4 | 6, token: string, socketId: string) => {
-	try {
-		await got.post(`${config.get<string>('api.httpHost')}/alternative-ip`, {
-			localAddress: ip,
-			dnsLookupIpVersion,
-			json: {
-				token,
-				socketId,
-			},
-			retry: {
-				limit: 1,
-				methods: [ 'POST' ],
-				statusCodes: [ 504 ],
-			},
-		});
-	} catch (e) {
-		logger.error((e as RequestError).message);
-	}
+	const httpHost = config.get<string>('api.httpHost');
+	const response = await got.post<{ ip: string }>(`${httpHost}/alternative-ip`, {
+		localAddress: ip,
+		dnsLookupIpVersion,
+		json: {
+			token,
+			socketId,
+		},
+		retry: {
+			limit: 1,
+			methods: [ 'POST' ],
+			statusCodes: [ 504 ],
+		},
+		responseType: 'json',
+	});
+
+	return response.body.ip;
 };
