7962a7763cba8b610c8654296c5fab09b19b0000
Martin Kolárik <martin@kolarik.sk>
2024-10-31 14:37:27 +0100

    misc: update message

--- src/helper/adoption-status-handler.ts
+++ src/helper/adoption-status-handler.ts
@@ -4,6 +4,6 @@ const logger = scopedLogger('api:connect:adoption');
 
 export const adoptionStatusHandler = async ({ isAdopted }: { isAdopted: boolean }): Promise<void> => {
 	if (!isAdopted) {
-		logger.info(`This probe can be registered at https://dash.globalping.io to earn measurement credits for you.`);
+		logger.info(`You can register this probe at https://dash.globalping.io to earn extra measurement credits.`);
 	}
 };
