87220bab6102971dae0713376397f22239581246
Martin Kolárik <martin@kolarik.sk>
2025-01-14 13:10:48 +0100

    fix: log message

--- src/helper/test-error-handler.ts
+++ src/helper/test-error-handler.ts
@@ -4,7 +4,7 @@ import { scopedLogger } from '../lib/logger.js';
 const logger = scopedLogger('test-error-handler');
 
 export const handleTestError = (error: unknown, socket: Socket, measurementId: string, testId: string) => {
-	logger.error('Failed to run the measurement.', error);
+	logger.error('Failed to run the measurement:', error);
 
 	const rawOutput = error instanceof Error ? error.toString() : String(error);
 
