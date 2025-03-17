2484faa50bf2eaf58f5ed22c02a79752c0c331c0
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-03-17 12:57:56 +0100

    feat: rename adoption token var

--- src/helper/adoption-token-handler.ts
+++ src/helper/adoption-token-handler.ts
@@ -4,11 +4,11 @@ import { scopedLogger } from '../lib/logger.js';
 const logger = scopedLogger('adoption-token-handler');
 
 export const adoptionTokenHandler = (socket: Socket) => {
-	const adoptionToken = process.env['ADOPTION_TOKEN'];
+	const adoptionToken = process.env['GP_ADOPTION_TOKEN'];
 
 	if (adoptionToken) {
 		socket.emit('probe:adoption:token', adoptionToken);
 	} else {
-		logger.info(`ADOPTION_TOKEN env var wasn't provided.`);
+		logger.info(`GP_ADOPTION_TOKEN env var wasn't provided.`);
 	}
 };
