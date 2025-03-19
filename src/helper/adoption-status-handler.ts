caed1c2e10fc61dc986b62c1ff8bad687a1d85c6
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-03-19 12:55:21 +0100

    feat: send log level to adoption status handler

--- src/helper/adoption-status-handler.ts
+++ src/helper/adoption-status-handler.ts
@@ -2,6 +2,6 @@ import { scopedLogger } from '../lib/logger.js';
 
 const logger = scopedLogger('api:connect:adoption');
 
-export const adoptionStatusHandler = async ({ message }: { message: string }): Promise<void> => {
-	logger.info(message);
+export const adoptionStatusHandler = async ({ message, level }: { message: string, level?: 'info' | 'warn' | 'error' }): Promise<void> => {
+	logger[level || 'info'](message);
 };
