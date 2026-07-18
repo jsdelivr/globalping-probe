bd6c0b8a3a84cfbb0a764cd277a2c3dad869f51f
Martin Kolárik <martin@kolarik.sk>
2026-07-18 17:37:39 +0200

    fix: install release preset with semantic-release

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,4 +1,18 @@
-const { DEFAULT_COMMIT_TYPES } = require('conventional-changelog-conventionalcommits');
+const releaseNoteTypes = [
+	{ type: 'feat', section: 'Features' },
+	{ type: 'feature', section: 'Features' },
+	{ type: 'fix', section: 'Bug Fixes' },
+	{ type: 'perf', section: 'Performance Improvements' },
+	{ type: 'revert', section: 'Reverts' },
+	{ type: 'docs', section: 'Documentation', hidden: true },
+	{ type: 'style', section: 'Styles', hidden: true },
+	{ type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+	{ type: 'refactor', section: 'Code Refactoring', hidden: true },
+	{ type: 'test', section: 'Tests', hidden: true },
+	{ type: 'build', section: 'Build System', hidden: true },
+	{ type: 'ci', section: 'Continuous Integration', hidden: true },
+	{ type: 'misc', section: 'Miscellaneous' },
+];
 
 module.exports = {
 	branches: [ 'master' ],
@@ -12,10 +26,7 @@ module.exports = {
 		[ '@semantic-release/release-notes-generator', {
 			preset: 'conventionalcommits',
 			presetConfig: {
-				types: [
-					...DEFAULT_COMMIT_TYPES,
-					{ type: 'misc', section: 'Miscellaneous' },
-				],
+				types: releaseNoteTypes,
 			},
 		}],
 		[ '@semantic-release/github', {
